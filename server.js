c7d0b03ea23e352054b722f9a4d48e17c99985f8
Nishant-coder-dotcom-max <nishantbasakofficial@gmail.com>
2025-08-22 19:35:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // MongoDB Connection
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/vintage-saree';
-mongoose.connect('mongodb://127.0.0.1:27017', {
+mongoose.connect(MONGODB_URI', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
